838bb768636d3413f026f6447ac9b734e713d98e
meshi <mehhi@yandex.ru>
2023-01-11 23:23:36 +0300

    select

--- client/src/components/Filters/VacancyListFilter.jsx
+++ client/src/components/Filters/VacancyListFilter.jsx
@@ -5,11 +5,18 @@ import axios from 'axios';
 import { FILTER_CITIES, FILTER_EXPIRIENCES, FILTER_GRADES, FILTER_SPECIALITIES, FILTER_STACKTOOLS } from "../../redux/types";
 
 const VacancyListFilter = (props) => {
+
+  const [selectedCity, setSelectedCity] = useState('');
   const dispatch = useDispatch();
-  
+  let citiesInFilter = useSelector(state => state.filterOptionsReducer.cities);
+
+  if (citiesInFilter === undefined) {
+    citiesInFilter = [];
+  }
+
   async function fetchFilterOptions(optionName) {
     const optionUrl = `http://localhost:8000/${optionName}`;
-    
+
     // setIsLoading(true);
     const response = await axios.get(optionUrl);
 
@@ -18,7 +25,12 @@ const VacancyListFilter = (props) => {
     const action = {type: '', payload: optionResults};
 
     switch (optionName) {
-      case 'cities':
+      // case 'cities':
+      //   action.type = FILTER_CITIES;
+      //   break;
+      case 'cities_by_list':
+        const citiesFilterResult = response.data.result;
+        action.payload = citiesFilterResult;
         action.type = FILTER_CITIES;
         break;
       case 'experiences':
@@ -40,7 +52,8 @@ const VacancyListFilter = (props) => {
   }
 
   useEffect(() => {
-    fetchFilterOptions('cities');
+    console.log('FETCH START')
+    fetchFilterOptions('cities_by_list');
     fetchFilterOptions('specialities');
     fetchFilterOptions('grades');
     fetchFilterOptions('experiences');
@@ -49,6 +62,12 @@ const VacancyListFilter = (props) => {
 
   return (
     <form className='vacancy-list-filter'>
+        <Select
+          options={[...citiesInFilter]}
+          value={selectedCity}
+          onChange={({ option }) => setSelectedCity(option)}
+          placeholder="Город..."
+        />
       {/* <Select
         options={['Меньше 1 года', 'От 1 до 3 лет', 'Больше 3 лет']}
         value={}
@@ -66,12 +85,6 @@ const VacancyListFilter = (props) => {
         value={valueSpeciality}
         onChange={({ option }) => setValueSpeciality(option)}
         placeholder="Специализация..."
-      />
-      <Select
-        options={[...cityList]}
-        value={valueSpeciality}
-        onChange={({ option }) => setValueSpeciality(option)}
-        placeholder="Город..."
       />
       <Button primary label="Filter" onClick={() => fetchVacanciesByOptions()} /> */}
     </form>
