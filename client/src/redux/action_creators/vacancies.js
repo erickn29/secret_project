5458bd2c190cd6183d21ef8f65c1fba97f572a69
meshi <mehhi@yandex.ru>
2023-01-21 16:40:46 +0300

    filters-ready-to-optimize

--- client/src/redux/action_creators/vacancies.js
+++ client/src/redux/action_creators/vacancies.js
@@ -7,17 +7,51 @@ export const fetchVacancies = (page = 1, countOnPage = 10, filterData) => {
     dispatch({type: GET_GENERAL_VACANCIES,});
 
     console.log(filterData);
-    let response;
+
+    // const generateReqParams = (filterData) => {
+    //   const queryParams = {};
+
+    //   queryParams.page = page;
+
+    //   if ('city' in filterData && filterData.city) {
+    //     queryParams.location = filterData.city;
+    //   }
+
+    //   if ('chosenSalaryFrom' in filterData && filterData.chosenSalaryFrom) {
+    //     queryParams.salary_from = filterData.chosenSalaryFrom;
+    //   }
+
+    //   if ('isRemote' in filterData  && filterData.isRemote) {
+    //     queryParams.is_remote = filterData.isRemote === true ? 1 : 0;
+    //   }
+
+    //   if ('experience' in filterData && filterData.experience) {
+    //     queryParams.experience = filterData.experience.join(',');
+    //   }
+
+    //   if ('speciality' in filterData  && filterData.speciality) {
+    //     queryParams.speciality = filterData.speciality.join(',');
+    //   }
+
+    //   if ('grade' in filterData  && filterData.grade) {
+    //     queryParams.grade = filterData.grade.join(',');
+    //   }
+
+    //   return queryParams;
+    // };
 
     try {
-      // if (true) {
-      //   response = await axios.get(ALL_VACANCIES_API_URL, { params: { page: page,  } } );
-      // } 
-      
-      if (true) {
-        response = await axios.get(FILTER_VACANCIES_API_URL, { params: { page: page, location: filterData.city, salary_from: filterData.chosenSalaryFrom, is_remote: filterData.isRemote === true ? 1 : 0 } } );
-      }
-      
+
+      let response = await axios.get(FILTER_VACANCIES_API_URL, { params: { 
+        page: page, 
+        location: filterData.city, 
+        salary_from: filterData.chosenSalaryFrom ? filterData.chosenSalaryFrom : null, 
+        is_remote: filterData.isRemote ? (filterData.isRemote === true ? 1 : 0) : null,
+        experience: filterData.experience ? filterData.experience.join(',') : null,
+        speciality: filterData.speciality ? filterData.speciality.join(',') : null,
+        grade: filterData.grade ? filterData.grade.join(',') : null,
+      } } );
+   
       dispatch({type: GET_GENERAL_VACANCIES_SUCCESS, 
         payload: {
           vacancies: response.data.results,
