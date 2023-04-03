cc2420137ecaf3714f5222d349df08c3be7d6d83
Yaroslav <arhdomain@gmail.com>
2023-04-03 21:55:42 +0300

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/components/*.{html}", "./templates/*.{html}", "./static/**/*.{js}"],
+  content: ["./templates/components/*.html", "./templates/*.html", "./static/**/*.js"],
   theme: {
     extend: {},
   },
   plugins: [],
-  darkMode: 'class',
+  // darkMode: 'class',
 }
 
